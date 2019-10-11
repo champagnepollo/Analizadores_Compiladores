372b693d1d8914ea8dbc1a606125882dbee492ff
Ivadios <ramireztiradojose@gmail.com>
2019-10-10 19:20:00 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        AFD analizador = new AFD("archivo.txt");
+        //analizador.imprimir();
+    }
+}
